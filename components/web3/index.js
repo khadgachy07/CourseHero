173490381d92e2dafdb06079ea9928070f532a6a
khadgachy07 <khadgachy07@gmail.com>
2022-05-18 17:40:24 +0545

    UI with component based

---
+++ components/web3/index.js
@@ -0,0 +1,5 @@
+import { fromJSON } from 'postcss';
+
+
+export {default as WalletBar} from './walletbar';
+export {default as EthRates} from './ethRates'
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      maxWidth: {
+        "8xl": "1920px"
+      },
+    },
+  },
+  plugins: [],
+}
