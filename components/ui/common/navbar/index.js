b2a25be83927a57dc5ee86727701bf9e3ee7c37a
khadgachy07 <khadgachy07@gmail.com>
2022-08-12 20:14:24 +0545

    Marketplace setup and hidden address

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { useWeb3 } from "@components/providers";
 import Button from "@components/ui/common/button";
-import { useAccount } from "@components/web3/hooks/useAccount";
+import { useAccount } from "@components/hooks/web3/useAccount";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { connect, isWeb3Loaded, isLoading } = useWeb3();
   const { account } = useAccount();
+  const {pathname} = useRouter();
+
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
@@ -17,7 +20,7 @@ export default function Navbar() {
                   Home
                 </a>
               </Link>
-              <Link href="/">
+              <Link href="/marketplace">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Marketplace
                 </a>
@@ -62,7 +65,9 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      {account.data && (
+      {account.data && 
+        !pathname.includes('/marketplace') &&
+      (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
             {account.data}
