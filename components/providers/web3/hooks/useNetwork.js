3959cda9ef5828d14232e6c0b0c7bf19fd9ebea1
khadgachy07 <khadgachy07@gmail.com>
2022-08-13 17:05:33 +0545

    Chain Name Included

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,19 +1,31 @@
 import { useEffect } from "react";
 import useSWR, { mutate } from "swr";
-import Web3 from "web3";
+
+const NETWORK = {
+  1 : "Ethereum Main Network",
+  3 : "Ropsten Test Network",
+  4 : "Rinkeby Test Network",
+  5 : "Goerli Test Network",
+  42 : "Kovan Test Network",
+  80001 : "Polygon Test Network",
+  97: "Binance Test Network",
+  1337: "Ganache"
+}
 
 export const handler = (web3, provider) => () => {
   const {mutate, ...rest} = useSWR(
     () => (web3 ? "web3/network" : null),
     async () => {
       const chainID = await web3.eth.getChainId();
-      return chainID;
+      return NETWORK[chainID];
     }
   );
 
   useEffect(() => {
     provider && 
-    provider.on("chainChanged", chainID => mutate(chainID) )
+    provider.on("chainChanged", chainID => {
+      mutate(NETWORK[parseInt(chainID,16)])
+    })
   })
 
   return {
