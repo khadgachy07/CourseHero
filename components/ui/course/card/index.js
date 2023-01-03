c55c73c47808183c1a10339f19ac1ca841304109
khadgachy07 <khadgachy07@gmail.com>
2023-01-03 14:18:35 +0545

    Purchase Model setup

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,33 +1,42 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Card({ course }) {
+export default function Card({course, Footer}) {
   return (
-    <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div
+      className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="flex h-full">
-        <div className="flex h-full">
+        <div className="flex-1 h-full next-image-wrapper">
           <Image
             className="object-cover"
-            layout="fixed"
-            width="200px"
-            height="230px"
             src={course.coverImage}
+            layout="responsive"
+            width="200"
+            height='300'
             alt={course.title}
+            priority= 'false'
           />
         </div>
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+        <div className="p-8 pb-4 flex-2">
+          <div
+            className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
             {course.type}
           </div>
-
           <Link href={`/courses/${course.slug}`}>
-            <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <a
+              className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline">
               {course.title}
             </a>
           </Link>
-          <p className="mt-2 text-gray-500">{course.description}</p>
+          <p
+            className="mt-2 text-gray-500">
+            {course.description.substring(0, 70)}...
+          </p>
+          { Footer &&
+            <Footer />
+          }
         </div>
       </div>
     </div>
-  );
+  )
 }
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -3,8 +3,12 @@ import { CourseCard, CourseList } from "@components/ui/course";
 import { getAllCourses } from "@content/courses/fetcher";
 import { WalletBar } from "@components/ui/web3";
 import { useAccount, useNetwork } from "@components/hooks/web3";
+import { Button } from "@components/ui/common";
+import { OrderModal } from "@components/ui/order";
+import { useState } from "react";
 
 export default function Marketplace({ courses }) {
+  const [selectedCourse, setSelectedCourse] = useState(null);
   const { account } = useAccount();
   const { network } = useNetwork();
 
@@ -22,8 +26,29 @@ export default function Marketplace({ courses }) {
         />
       </div>
       <CourseList courses={courses}>
-        {(course) => <CourseCard key={course.id} course={course} />}
+        {(course) => (
+          <CourseCard
+            key={course.id}
+            course={course}
+            Footer={() => (
+              <div className="mt-4">
+                <Button 
+                  onClick = {() => setSelectedCourse(course)}
+                  variant="lightPurple">Purchase
+                </Button>
+              </div>
+            )}
+          />
+        )}
       </CourseList>
+      {
+        selectedCourse && 
+        <OrderModal 
+          course={selectedCourse}
+          onClose= {() => setSelectedCourse(null)}
+        />
+      }
+      
     </>
   );
 }
